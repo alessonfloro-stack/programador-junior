a784bd2926a775a8a0d8e087ae37cb7a2b050e79
amazingdw <alessonfloro@gmail.com>
2026-09-02 10:30:07 -0300

    Adiciona projeto diário com datetime e arquivos

---
+++ diario.py
@@ -0,0 +1,22 @@
+import datetime
+#PROJETO DIÁRIO:
+
+#LOOP:
+while True:
+    #IMPUT DE DADOS:
+    pergunta = input("Escreva o que quiser: ")
+    #SAIR?
+    if pergunta == "sair":
+        break
+    #VARÍAVEL DE TEMPO:
+    tempo = datetime.datetime.now()
+    tempo_formatado = tempo.strftime("%d/%m/%Y %H:%M")
+    #ABRE O ARQUIVO PARA ADICIONAR DADOS:
+    with open("diario.txt", "a") as arquivo:
+        #ESCREVE NO ARQUIVO:
+        arquivo.write(f"{tempo_formatado}: {pergunta}\n")
+        #MOSTRA QUE FOI SALVO:
+        print("Dados Salvos no seu diário.")
+with open("diario.txt", "r") as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
